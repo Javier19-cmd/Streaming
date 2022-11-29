8e18cd5e4514f8e94084bf239bcc9bbd78e17bc2
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-11-28 19:32:43 -0600

    creando el servidor

---
+++ server/index.js
@@ -0,0 +1,19 @@
+/**
+ * Referncias: 
+ * Curso PERN en React: https://www.youtube.com/watch?v=ldYcgPKEZC8
+ * Usando nodemon: https://www.youtube.com/watch?v=Uw3faNl_O0w
+ * 
+*/
+
+// Importando express.
+import express from 'express'
+import cors from 'cors'
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(cors()) // Configuarando CORS.
+app.use(express.json()) // Configuración de body-server.
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
